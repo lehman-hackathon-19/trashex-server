ae7e6f013be5925377c2c7e1fbfcbcbbc84dc49d
Aswin Nair <aswintorch@gmail.com>
2019-11-16 10:34:16 -0500

    Fixed the worst damn bug ever

--- routes/users.js
+++ routes/users.js
@@ -8,12 +8,27 @@ router.get("/", function(req, res, next) {
   res.send("respond with a resource");
 });
 
-router.put("/:id", function(req, res, next) {
-  User.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
-    User.findOne({ _id: req.params.id }).then(function(user) {
-      res.send(user);
-    });
+router.get("/:id", function(req, res, next) {
+  User.findOne({ _id: req.params.id }).then(function(user) {
+    res.send(user);
   });
+});
+
+router.put("/:id", async function(req, res, next) {
+  const existing = (await  User.findOne({ _id: req.params.id }));
+  console.log(existing);
+  try {
+    existing.totalCount = existing.totalCount + req.body.totalCount;
+    console.log(req.body);
+    const existingUser = await User.findByIdAndUpdate({ _id: req.params.id }, req.body);
+    existingUser.set(existing);
+    const result = await existingUser.save();
+    res.send(result)
+  } catch (err) {
+    next(err);
+  }
+
+  
 });
 
 module.exports = router;
